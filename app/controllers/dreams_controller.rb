e0ff403cd3c4fb50fad1ced92fd7d19cfea1bc29
Sean Sylvis <ssylvis@gmail.com>
2013-09-29 14:29:23 -0700

    Mark dreams as completed

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,5 +1,6 @@
 class DreamsController < ApplicationController
   before_filter :login_required
+  before_filter :parse_completed, :only => :update
 
   def create
     @dream = current_user.dreams.build(params[:dream])
@@ -33,7 +34,7 @@ class DreamsController < ApplicationController
   def update
     @dream = find_dream(params[:id])
     if @dream.update_attributes(params[:dream])
-      redirect_to dreams_url
+      redirect_to dreams_url(params.slice(:edit, :state))
     else
       render 'edit'
     end
@@ -43,5 +44,13 @@ private
 
   def find_dream(id)
     current_user.dreams.find(id)
+  end
+
+  def parse_completed
+    dream = params[:dream]
+    if dream[:completed].present?
+      completed_at = Time.now if (dream.delete(:completed) == 'true')
+      dream[:completed_at] = completed_at
+    end
   end
 end
