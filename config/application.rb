67ed70ed3d8376ebd6720c433cc52a170faefac9
Sean Sylvis <ssylvis@gmail.com>
2013-12-10 21:20:25 -0800

    Remove precompiled assets and fix application config for Heroku

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,9 @@ module Dreamgram
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Do not initialize the application on precompile
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
