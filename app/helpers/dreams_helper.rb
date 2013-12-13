ee09b74763399ecf7d387d231f0f764e3ce9e0f5
Sean Sylvis <ssylvis@gmail.com>
2013-12-12 22:00:39 -0800

    Add title with completed time to image

--- app/helpers/dreams_helper.rb
+++ app/helpers/dreams_helper.rb
@@ -2,6 +2,14 @@ module DreamsHelper
   IMAGE_HEIGHT = 330
   IMAGE_WIDTH = 220
 
+  def dream_image_title(dream)
+    if dream.completed_at
+      "#{dream.description} (completed #{distance_of_time_in_words_to_now(dream.completed_at)} ago)"
+    else
+      dream.description
+    end
+  end
+
   def image_style(dream)
     if dream.cropped?
       ratio = IMAGE_HEIGHT / dream.crop_h.to_f
