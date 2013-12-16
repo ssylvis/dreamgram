2e54060987568e14ffdb4d67c6647f84e607b68b
Sean Sylvis <ssylvis@gmail.com>
2013-12-15 23:02:52 -0800

    Change to double quotes in JS files

--- app/assets/javascripts/crop.js
+++ app/assets/javascripts/crop.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   var IMAGE_HEIGHT = 330
   var IMAGE_WIDTH = 220
 
-  var crop_x = $('#crop_x').val() || 0;
-  var crop_y = $('#crop_y').val() || 0;
-  var crop_w = $('#crop_w').val() || IMAGE_WIDTH;
-  var crop_h = $('#crop_h').val() || IMAGE_HEIGHT;
+  var crop_x = $("#crop_x").val() || 0;
+  var crop_y = $("#crop_y").val() || 0;
+  var crop_w = $("#crop_w").val() || IMAGE_WIDTH;
+  var crop_h = $("#crop_h").val() || IMAGE_HEIGHT;
 
-  $('#cropbox').Jcrop({
+  $("#cropbox").Jcrop({
     onChange: updateCrop,
     onSelect: updateCrop,
     setSelect: [ crop_x, crop_y, crop_x + crop_w, crop_y + crop_h ],
@@ -16,8 +16,8 @@ $(document).ready(function() {
 });
 
 function updateCrop(coords) {
-  $('#crop_x').val(coords.x);
-  $('#crop_y').val(coords.y);
-  $('#crop_w').val(coords.w);
-  $('#crop_h').val(coords.h);
+  $("#crop_x").val(coords.x);
+  $("#crop_y").val(coords.y);
+  $("#crop_w").val(coords.w);
+  $("#crop_h").val(coords.h);
 }
--- app/assets/javascripts/dream.js
+++ app/assets/javascripts/dream.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   var SHOW_DELAY = 25;
-  $('.dream').hover(function() {
-    $(this).find('.edit-buttons').not('.show').show(SHOW_DELAY);
+  $(".dream").hover(function() {
+    $(this).find(".edit-buttons").not(".show").show(SHOW_DELAY);
   }, function() {
-    $(this).find('.edit-buttons').not('.show').hide(SHOW_DELAY);
+    $(this).find(".edit-buttons").not(".show").hide(SHOW_DELAY);
   });
 });
