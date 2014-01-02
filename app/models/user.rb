88c7baeb7b3613c2cfc3a15c57eb1f8374f6a5b0
Sean Sylvis <sean@cardspring.com>
2014-01-01 21:05:37 -0800

    Add recoverable capability to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :registerable, :rememberable, :validatable
 
   has_many :dreams, :dependent => :destroy
 
-  attr_accessible :email, :name
+  attr_accessible :email
+  attr_accessible :name
   attr_accessible :password, :password_confirmation
   attr_accessible :remember_me
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131231043357) do
+ActiveRecord::Schema.define(:version => 20140102045509) do
 
   create_table "dreams", :force => true do |t|
     t.string   "description",                 :null => false
@@ -29,14 +29,17 @@ ActiveRecord::Schema.define(:version => 20131231043357) do
   add_index "dreams", ["user_id"], :name => "index_dreams_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",               :null => false
-    t.string   "name",                :null => false
-    t.datetime "created_at",          :null => false
-    t.datetime "updated_at",          :null => false
-    t.string   "encrypted_password",  :null => false
+    t.string   "email",                  :null => false
+    t.string   "name",                   :null => false
+    t.datetime "created_at",             :null => false
+    t.datetime "updated_at",             :null => false
+    t.string   "encrypted_password",     :null => false
     t.datetime "remember_created_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
