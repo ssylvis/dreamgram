18fd9a6e6fda40e65f5ab3ae207c013faef1bb67
Sean Sylvis <sean@cardspring.com>
2014-01-05 22:47:24 -0800

    Add sanitizer for additional signup parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  UPDATE_PARAMS = [:name]
+  SIGN_UP_PARAMS = [:name, :remember_me]
+
   protect_from_forgery
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update).push(*UPDATE_PARAMS)
+    devise_parameter_sanitizer.for(:sign_up).push(*SIGN_UP_PARAMS)
+  end
 end
