15d917ec63fe4865b70113ff74ddea747fd2a384
Sean Sylvis <sean@cardspring.com>
2014-01-21 21:37:55 -0800

    Add dismissable functionality to notices

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require jquery.Jcrop.min.js
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def dismiss_button
+    haml_tag :button, :type => "button", :class => "close", :"data-dismiss" => "alert" do
+      haml_concat "&times;".html_safe
+    end
+  end
+
   def page_title(page)
     title = t('layouts.title')
     page.present? ? "#{title} - #{page}" : title
