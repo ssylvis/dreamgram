298195efac44d3c56cb653a1c5e37f68f798b42d
Sean Sylvis <sean@cardspring.com>
2014-01-23 22:26:17 -0800

    Revert AJAX for account update until form feedback works

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  respond_to :html, :js
-
 protected
 
   def after_inactive_sign_up_path_for(resource)
@@ -9,17 +7,15 @@ protected
   end
 
   def after_update_path_for(resource)
-    dreams_path
+    edit_account_registration_path
   end
 
   def update_resource(resource, params)
-    updated =
-      if needs_password?(params)
-        resource.update_with_password(params)
-      else
-        resource.update_without_password(params)
-      end
-    flash.now[:notice] = find_message(:updated) if updated
+    if needs_password?(params)
+      resource.update_with_password(params)
+    else
+      resource.update_without_password(params)
+    end
   end
 
 private
