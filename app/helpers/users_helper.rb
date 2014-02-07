e28c0b79da5bbbf8e9ac9cfb6b251ce975cd0b77
Sean Sylvis <sean@cardspring.com>
2014-02-06 21:19:10 -0800

    Fixup params for facebook sharing

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,27 +1,25 @@
 module UsersHelper
   def facebook_share_link(account)
+    host = 'https://www.facebook.com/dialog/feed'
+    dream = account.dreams.first
     url = user_url(:uid => account.uid)
-    if Rails.env.production?
-      host = 'https://www.facebook.com/dialog/feed'
-      params = {
-        :app_id => '1387078131558211',
-        :description => 'Check out my bucketlist on Dreamgram!',
-        :display => 'popup',
-        :link => url,
-        :name => 'Dreamgram',
-        :url => url
-      }
-    else
-      host = 'https://www.facebook.com/sharer/sharer.php'
-      params = { :u => url }
-    end
+    params = {
+      :app_id => '1387078131558211',
+      :caption => dream.description,
+      :description => t('users.social.description'),
+      :display => 'popup',
+      :link => url,
+      :name => t('users.social.name'),
+      :picture => dream.image,
+      :redirect_uri => url
+    }
     "#{host}?#{params.to_param}"
   end
 
   def twitter_share_link(account)
     host = 'https://twitter.com/share'
     params = {
-      :text => 'Check out my bucketlist on Dreamgram!',
+      :text => t('users.social.description'),
       :url => user_url(:uid => account.uid)
     }
     "#{host}?#{params.to_param}"
