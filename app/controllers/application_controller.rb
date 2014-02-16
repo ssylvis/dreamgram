c5539f92620e48a0836ed525ea3c5089cbc5461f
Sean Sylvis <sean@cardspring.com>
2014-02-16 10:05:32 -0800

    Add record_not_found to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   include Application::DeviseHandling
 
   protect_from_forgery
+
+  protected
+
+  def record_not_found
+    raise ActiveRecord::RecordNotFound
+  end
 end
