29c743557b44cb6951c9cf8d0b055ef926a8e081
Sean Sylvis <sean@cardspring.com>
2014-03-13 21:07:03 -0700

    Add notice when re-registering

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -13,25 +13,27 @@ class AuthenticationController < Devise::OmniauthCallbacksController
   private
 
   def authenticate_request(provider)
+    provider_name = provider.to_s.capitalize
     auth = request.env['omniauth.auth']
     # sign in existing accounts whether login or signup request
     if oauth = Oauth.find_by_provider(auth.provider, auth.uid)
+      set_flash(:notice, :already_registered, :provider => provider_name) unless login_request?
       account = oauth.account
       remember_me account
       sign_in_and_redirect account
     else
       if login_request?
-        set_flash_message(:alert, :invalid) if is_flashing_format?
+        set_flash(:alert, :invalid)
         redirect_to new_account_session_path
       else
-        # check whether we have an existing email account
         self.class.save_oauth_data(session, auth)
+        # check whether we have an existing account
         if account = Account.find_by_email(auth.info.email)
+          set_flash(:notice, :already_registered, :provider => existing_providers(account))
           account.oauths.create!(oauth_create_params(account))
           sign_in_and_redirect account
         else
-          provider_name = provider.to_s.capitalize
-          set_flash_message(:notice, :oauth_signup, :provider => provider_name) if is_flashing_format?
+          set_flash(:notice, :oauth_signup, :provider => provider_name)
           redirect_to new_account_registration_path
         end
       end
@@ -40,11 +42,19 @@ class AuthenticationController < Devise::OmniauthCallbacksController
 
   private
 
+  def existing_providers(account)
+    account.oauths.map { |oauth| oauth.provider.to_s.capitalize }.join(', ') || 'email'
+  end
+
   def login_request?
     request.env['omniauth.origin'] == new_account_session_url
   end
 
   def oauth_create_params(account)
     self.class.oauth_data(session).slice(:provider, :provider_uid).merge(:account => account)
+  end
+
+  def set_flash(key, kind, args = {})
+    set_flash_message(key, kind, args) if is_flashing_format?
   end
 end
