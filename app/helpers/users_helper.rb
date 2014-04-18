cc8e3b3e5842ea8804da93d7311b0445e203196f
Sean Sylvis <sean@cardspring.com>
2014-04-17 22:40:41 -0700

    Whitespace

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def facebook_share_link(account)
     host = 'https://www.facebook.com/sharer/sharer.php'
-    params = {:u => user_url(:uid => account.uid)}
+    params = { :u => user_url(:uid => account.uid) }
     "#{host}?#{params.to_param}"
   end
 
