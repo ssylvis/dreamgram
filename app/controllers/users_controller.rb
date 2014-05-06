ddaa0ab5099bed67fb80cb9919a920ad5076ac2f
Sean Sylvis <sean@cardspring.com>
2014-05-05 22:00:27 -0700

    Revert section showing linkable dreams of others

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,10 @@
 class UsersController < ApplicationController
-  NUM_FRIENDS_SHOWN = 3
-
   before_filter :find_account
 
   def show
     @dreams = @account.dreams
     @active_dreams = @dreams.active.order_by_created
     @completed_dreams = @dreams.completed.order_by_completed
-    friends_active_dreams = @account.friends.map { |friend| friend.dreams.active.first }.compact
-    @friends_dreams = friends_active_dreams.sample(NUM_FRIENDS_SHOWN)
   end
 
   private
